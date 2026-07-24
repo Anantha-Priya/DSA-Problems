63dd2f9695518fea5791ab0f0d06b038f3b8a507
Anantha-Priya <priyasolairaj133@gmail.com0>
2026-07-24 22:17:01 +0530

    Binary Search Problem

---
+++ Binary Search/missingElement2.java
@@ -0,0 +1,21 @@
+public class missingElement2 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,6,7,8};
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == mid + 1) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        System.out.println(low + 1);
+    }
+}
